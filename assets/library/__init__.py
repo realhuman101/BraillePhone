d51bb709fdf2f5f8d5d53b8712f704aaf86b3ab1
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-06-24 02:10:27 +0800

    Fixed frequency

--- assets/library/__init__.py
+++ assets/library/__init__.py
@@ -54,9 +54,9 @@ class Controller:
 			GPIO.setup(15, GPIO.OUT)
 
 			# Set up pins
-			TopPin = GPIO.PWM(11, 0) # Top servo
-			MiddlePin = GPIO.PWM(13, 0) # Middle
-			BottomPin = GPIO.PWM(15, 0) # Bottom
+			TopPin = GPIO.PWM(11, 50) # Top servo
+			MiddlePin = GPIO.PWM(13, 50) # Middle
+			BottomPin = GPIO.PWM(15, 50) # Bottom
 
 			TopPin.start(0)
 			MiddlePin.start(0)
