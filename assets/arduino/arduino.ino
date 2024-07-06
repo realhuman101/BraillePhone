abd95e5dad2236f08e0f99d9d9f9d1cc628a70ee
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-06 16:55:17 +0800

    Connect servos to pins

--- assets/arduino/arduino.ino
+++ assets/arduino/arduino.ino
@@ -1,11 +1,23 @@
 // Used to connect the servos to the arduino, which will in turn be used by the raspberry pi to connect to the servos
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+// Servos
+Servo top;  // Top servo
+Servo mid;  // Middle servo
+Servo btm;  // Bottom servo
+
+// PWN pins
+const int topPin = 8;
+const int midPin = 9;
+const int btmPin = 10;
+
+void setup() {
+  top.attach(topPin);  // Attach top servo to pin
+  mid.attach(midPin);  // Attach middle servo to pin
+  btm.attach(btmPin);  // Attach bottom servo to pin
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Main code
 }
