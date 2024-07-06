961365a048779de874ef6c6e03c5128396aaa3ba
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-06 17:59:10 +0800

    add movement handler

--- assets/arduino/arduino.ino
+++ assets/arduino/arduino.ino
@@ -30,14 +30,25 @@ void setup() {
 }
 
 void loop() {
-  int inpTP1 = analogRead(A0); // Input #1 for top servo
-  int inpTP2 = analogRead(A1); // Input #2 for top servo
+  float inpTP1 = analogRead(A0) * (5.0 / 1023.0); // Input Voltage #1 for top servo
+  float inpTP2 = analogRead(A1) * (5.0 / 1023.0); // Input Voltage #2 for top servo
 
-  int inpMP1 = analogRead(A2); // Input #1 for middle servo
-  int inpMP2 = analogRead(A3); // Input #2 for middle servo
+  float inpMP1 = analogRead(A2) * (5.0 / 1023.0); // Input Voltage #1 for middle servo
+  float inpMP2 = analogRead(A3) * (5.0 / 1023.0); // Input Voltage #2 for middle servo
   
-  int inpBP1 = analogRead(A4); // Input #1 for bottom servo
-  int inpBP2 = analogRead(A5); // Input #2 for bottom servo
+  float inpBP1 = analogRead(A4) * (5.0 / 1023.0); // Input Voltage #1 for bottom servo
+  float inpBP2 = analogRead(A5) * (5.0 / 1023.0); // Input Voltage #2 for bottom servo
+
+  /* Note that '* (5.0 / 1023.0)' converts the analogue value to a voltage */
+
+  // Handle top
+  movementHandler(inpTP1, inpTP2, top);
+
+  // Handle middle
+  movementHandler(inpMP1, inpMP2, mid);
+
+  // Handle bottom
+  movementHandler(inpBP1, inpBP2, btm);
 }
 
 // Servo rotation function
@@ -46,4 +57,27 @@ void rotateServo(Servo selServo, int pos) {
     Servo selServo = Selected servo to rotate
     int pos = Position (varying from 1-3) to move in, where 1 is left, 2 is middle and 3 is right 
   */
+}
+
+// Input to movement conversion function
+void movementHandler(float pin1, float pin2, Servo selServo) {
+  /*
+    float pin1 = First input
+    float pin2 = Second input
+    Servo selServo = Selected servo
+  */
+
+  if (pin1 >= 5 && pin2 >= 5) { // Middle
+
+    rotateServo(selServo, 2);
+
+  } else if (pin1 >= 5) { // Left
+
+    rotateServo(selServo, 1);
+
+  } else if (pin2 >= 5) { // Top
+
+    rotateServo(selServo, 3); 
+
+  }
 }
