b508ad1e946fc701ada46844ca2d6cdeef8a7de3
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-06 16:48:37 +0800

    add base arduino file

---
+++ assets/arduino/arduino.ino
@@ -0,0 +1,11 @@
+// Used to connect the servos to the arduino, which will in turn be used by the raspberry pi to connect to the servos
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
